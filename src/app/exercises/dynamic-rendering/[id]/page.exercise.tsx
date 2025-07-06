beabec83b8384c98e976cc0c4ad2e085d24ffaf0
Jaem Dessources <jaem.developer@gmail.com>
2025-07-06 19:28:12 -0400

    completed chapter 2

--- src/app/exercises/dynamic-rendering/[id]/page.exercise.tsx
+++ src/app/exercises/dynamic-rendering/[id]/page.exercise.tsx
@@ -1,13 +1,45 @@
 import RenderTime from '@/components/render-time'
 // 🐶 Importe `getPostById` la fonction qui accède à la BDD.
-// 🤖 import {getPostById} from '@/db/sgbd'
+import {getPostById, getPosts} from '@/db/sgbd'
+import {Post} from '@/lib/type'
+import {Metadata} from 'next'
+import {notFound} from 'next/navigation'
+
+export const revalidate = 10
+export async function generateStaticParams() {
+  return await getPosts().then((posts) =>
+    posts?.map(({id}) => ({
+      id,
+    }))
+  )
+}
+
+export async function generateMetadata(props: {
+  params: Promise<{id: string}>
+}): Promise<Metadata> {
+  const params = await props.params
+  const post: Post = (await getPostById(params.id)) as Post
+  if (!post)
+    return {
+      title: 'Post Not Found',
+      description: 'This post does not exist.',
+    }
+  else
+    return {
+      title: post.title,
+      description: post.title,
+      openGraph: {
+        title: post.title,
+      },
+    }
+}
 
 const Page = async (props: {params: Promise<{id: string}>}) => {
   const params = await props.params //next 15
   //🐶 Remplace ce tableau par l'appel à la fonction `getPostById` avec l'ID de l'article.
-  const post = {id: `${params.id}`, title: 'FAKE POST'}
+  const post = await getPostById(params.id)
   // 🐶 Affiche une page 404 si l'id de post n'existe pas en BDD
-  // 🤖 if (!post) notFound()
+  if (!post) notFound()
 
   return (
     <div className="mx-auto max-w-4xl p-6 text-lg">
--- src/app/exercises/dynamic-rendering/search-params/page.exercise.tsx
+++ src/app/exercises/dynamic-rendering/search-params/page.exercise.tsx
@@ -1,29 +1,41 @@
 import RenderTime from '@/components/render-time'
 import {getPosts} from '@/db/sgbd'
 import {Post} from '@/lib/type'
+import {notFound} from 'next/navigation'
 
 // 🐶 Cette page est statique par défaut
 // 🐶 par rapport à l'exercice 3. 🚀 Search Params
 
 // 🐶 Ajoute les `props` pour avoir accès à `searchParams`
 //https://nextjs.org/docs/app/api-reference/file-conventions/page#searchparams-optional
-const Page = async () => {
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{[key: string]: string | string[] | undefined}>
+}) => {
   const posts = await getPosts()
 
-  // 🐶 Récupère le champs à filter et la valeur grâce à :
+  const {filter: filterField, text: textField} = await searchParams
 
   // 🤖 const filterField = searchParams?.filter as string //champs à filrer
   // 🤖 const text = searchParams?.text as string //valeur à filrer
 
   // 🐶 Utilise la méthode `filter`
-  // 🤖 const filteredPosts = posts.filter
+  const filteredPosts = posts.filter((post) =>
+    post[filterField as keyof Post]
+      ?.toLowerCase()
+      .includes((textField as string).toLowerCase())
+  )
+  if (filteredPosts.length) notFound()
 
   return (
     <div className="mx-auto max-w-4xl p-6 text-lg">
       <h1 className="mb-4 text-center text-3xl font-bold">Search Posts</h1>
       <ul className="list-disc p-4 pl-4">
         {/* 🐶 Remplace `post` par `filteredPosts` */}
-        {posts?.map((post: Post) => <li key={post.id}>{post.title}</li>)}
+        {filteredPosts?.map((post: Post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
       </ul>
       <RenderTime />
     </div>
