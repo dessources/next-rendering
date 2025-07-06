920f0e238965a9bd2d8a1b8d0e9d53aa831d7ca4
Jaem Dessources <jaem.developer@gmail.com>
2025-07-06 18:20:44 -0400

    completed chapter 1

--- src/app/exercises/static-rendering/page.exercise.tsx
+++ src/app/exercises/static-rendering/page.exercise.tsx
@@ -1,14 +1,14 @@
 import RenderTime from '@/components/render-time'
 
 // 🐶 Importe la fonction `getPosts` qui permet d'accéder à notre base de donées
-// 🤖 import {getPosts} from '@/db/sgbd'
+ import {getPosts} from '@/db/sgbd'
 import {Post} from '@/lib/type'
 
 // 🐶 Transforme ce composant en asynchrone pour pouvoir faire l'appel en BDD.
-const Page = () => {
+const Page =  async () => {
   // ⛏️ Remplace les posts statiques par les posts de la base de données, utilise
   // 🤖 getPosts()
-  const posts: Post[] = [{title: 'Un post coder dans le RSC'}]
+  const posts: Post[] = await getPosts();
 
   // 🐶 Teste le comportement en mode DEV et PRODUCTION
   return (
