8d9201f678b9af0711a238169ad846fbb6e5e8fe
Jaem Dessources <jaem.developer@gmail.com>
2025-07-09 15:19:26 -0400

    completed chapter 6

--- src/app/exercises/client-components/data.exercise.ts
+++ src/app/exercises/client-components/data.exercise.ts
@@ -1,4 +1,4 @@
-// 🐶 Fais en sorte que ce code ne puisse pas être exposé au client
+import 'server-only'
 export async function getData() {
   const secretKey = '#######_this_is_a_secret_key_#######'
   const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
--- src/app/exercises/client-components/page.exercise.tsx
+++ src/app/exercises/client-components/page.exercise.tsx
@@ -2,7 +2,9 @@ import RenderTime from '@/components/render-time'
 import {getPosts} from '@/db/sgbd'
 
 // 🐶 Importe le `RCC counter`
-//import {Counter} from './rcc-counter'
+import {Counter} from './rcc-counter'
+import ExternalLib from './client-lib-wrapper'
+import RccFetch from './rcc-fetch'
 
 export default async function Page() {
   const posts = await getPosts()
@@ -14,8 +16,9 @@ export default async function Page() {
       <h2 className="mb-4 text-center text-2xl font-bold">
         il y a {posts.length} posts
       </h2>
-      {/* 🐶 Appelle ici le composant <Counter>*/}
-      {/* 🐶 En cas de problème, va corriger le composant counter*/}
+      <Counter />
+      <ExternalLib defaultDelay={100} />
+
       <RenderTime name="Page" />
     </div>
   )
